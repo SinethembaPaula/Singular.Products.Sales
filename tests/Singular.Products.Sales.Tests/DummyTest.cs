bc102b240000d7cdccd5b16fd6141c83e96f9751
Sinethemba Paula <teepaula6@gmail.com>
2025-07-15 09:48:21 +0200

    Adding a dummy test

---
+++ tests/Singular.Products.Sales.Tests/DummyTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Singular.Products.Sales.Tests
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
